a80c749517c28d92e1416a0b178c565c827e2525
Warren Siro <warrensiro@gmail.com>
2024-05-17 01:46:31 +0300

    changes to static settings

--- study/settings.py
+++ study/settings.py
@@ -122,14 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'images/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIR = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'image/images'
 
 
 # Default primary key field type
