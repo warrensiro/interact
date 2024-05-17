be03cff64ff99cd777574b73521588d9150c7443
Warren Siro <warrensiro@gmail.com>
2024-05-17 14:17:53 +0300

    changed the static path

--- study/settings.py
+++ study/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIR = [
-    BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'MEDIA'
 
 
 # Default primary key field type
