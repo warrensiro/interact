31a5f8f799ed28da1e81a2b2c16b3fb0bdf5bba9
Warren Siro <warrensiro@gmail.com>
2024-05-17 15:47:24 +0300

    adding all the changes

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
+
 
 urlpatterns = [
     path('login/password-reset/', auth_views.PasswordResetView.as_view
